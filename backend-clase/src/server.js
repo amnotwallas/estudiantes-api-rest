a1e2dec2870ab23ef2e419c99e274e7f1c445aa9
WalterAmbriz <walterardev@gmail.com>
2025-06-09 08:36:08 -0600

    refactor: cors

--- backend-clase/src/server.js
+++ backend-clase/src/server.js
@@ -7,7 +7,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
